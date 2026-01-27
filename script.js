55ad2262571c25b63acbafbe29f55f2d184c9d81
Anugamannn <158793709+Anugamannn@users.noreply.github.com>
2026-01-26 20:08:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on date the Aayusha, I'll bring orange juice!";
+  question.innerHTML = "Yay, see you on date the Aayusha!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
@@ -22,3 +22,4 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
 });
+
